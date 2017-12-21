abb1c30813eda3b3afaac1526ef2af0153aac7c0
TaoStarlit <zhengt2016@gmail.com>
2017-12-21 22:13:44 +1100

    save and restore paremeter with the global step

---
+++ tf1.3Project/0.9save_restore_global_step.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import os
+
+sess = tf.Session()
+print(sess)  # <tensorflow.python.client.session.Session object at 0x0000020EA43B5F60>
+
+W = tf.Variable([3], dtype=tf.float32)
+b = tf.Variable([-3], dtype=tf.float32)
+init = tf.global_variables_initializer()
+
+# <tf.Variable 'Variable:0' shape=(1,) dtype=float32_ref>  ... <tf.Operation 'init' type=NoOp>
+print(W, b, init)
+
+print(sess.run(init))
+
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+print(W, b, x)  # <tf.Variable 'Variable:0' shape=(1,) dtype=float32_ref> <tf.Variable 'Variable_1:0' shape=(1,) dtype=float32_ref> Tensor("Placeholder:0", dtype=float32)
+# linear model node: Tensor("add:0", dtype=float32)
+print("linear model node:", linear_model)
+
+
+# here we just use the variable, but don't train them
+# placeholder take list input, 3*1-3=0 ... 3*4-3=9
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+
+# using loss function to measures how far apart the current model is from the provided data.
+# least square is the common way to train linear model
+# square of (model output - expected value provided data)  let the sum minimum
+y = tf.placeholder(tf.float32)
+squared_delta = tf.square(linear_model - y)
+# can it train the variables, no! because you didn't use the optimizers
+loss = tf.reduce_sum(squared_delta)
+# (<tf.Tensor 'Square:0' shape=<unknown> dtype=float32>, <tf.Tensor 'Sum:0' shape=<unknown> dtype=float32>)
+print("squared_delta,loss: ", squared_delta, loss)
+# 0 3 6 9, so the sum will be around 2
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, 2, 7, 9]}))
+
+
+'''
+train
+'''
+# tensorflow provides optimizers that slowly change each variable in order to minimize the loss funcion.
+# The simplest optimizer is gradient descent. It modifies each variable according to the magnitude of the derivative of loss
+# with respect to that variable.   [magnitude:The great size or extent of something.]
+# bigger, and the train will be quicker !! if bigger than 0.025 it can not converge
+optimizer = tf.train.GradientDescentOptimizer(0.025)
+train = optimizer.minimize(loss)
+# (<tensorflow.python.training.gradient_descent.GradientDescentOptimizer object at 0x2b9cb35cc9d0>,
+print(optimizer)
+print(train)  # <tf.Operation 'GradientDescent' type=NoOp>)
+
+sess.run(init)
+fixW = tf.assign(W, [.3])
+fixb = tf.assign(b, [-.3])
+sess.run([fixW, fixb])
+print("W,b,x : ", W, b, x)
+print("W,b value before train: ", sess.run([W, b]))
+
+
+savepara_file = 'model.ckpt'  # NameError: name 'FLAGS' is not defined
+savepara_dir = './save_parameter/'
+start_checkpoint = savepara_dir + savepara_file + '-180'
+# start_checkpoint='' # if start_checkpoint  false
+
+# saver = tf.train.Saver({"v2": v2}) # I did not give the variable name here, so tf.global_variables()
+saver = tf.train.Saver(tf.global_variables())
+if not os.path.exists(savepara_dir):  # python 3 not 2
+    os.makedirs(savepara_dir)
+if start_checkpoint:  # NameError: name 'start_checkpoint' is not defined
+    saver.restore(sess, start_checkpoint)
+    print('restore the checkpoint:',start_checkpoint)
+    print("W,b value after restore: ", sess.run([W, b]))
+    #start_step = global_step.eval(session=sess)
+    ##global_step = tf.contrib.framework.get_or_create_global_step()
+else:
+    print("no save_parameter")
+
+if not start_checkpoint:
+    for i in range(200):
+        sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+        if(i % 20 == 0):
+            print("W,b:", sess.run([W, b]))
+            print('loss:', sess.run(
+                loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+            print("save the parameter!! in step", i)
+            saver.save(sess, savepara_dir + savepara_file, global_step=i)
+
+print("W,b,x : ", W, b, x)
+print("W,b after train or restore:", sess.run([W, b]))
+print('loss:', sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+'''
+how to set the right parameter of GradientDesecentOptimizer and the repetition of train.
+'''
--- tf1.3Project/plan.md
+++ tf1.3Project/plan.md
@@ -1,5 +1,42 @@
-# 1.visualize learning
+1. visualize learning
 
-# 1.save the trained parameter
+    finished: in windows, we can visualize it, even the inputdata mudual, has the tensors to visualize;
 
-# 1.save and check the input file, don't download every time
+1. save the trained parameter
+
+    reference: the train.py has save the parameter, and the freeze.py load the model and parameters;
+    modify the 0.2TrainableParameters.py 
+        <-- do the same experiment as the tutorial
+            https://www.tensorflow.org/api_docs/python/tf/train/Saver
+            https://www.tensorflow.org/programmers_guide/saved_model
+
+
+## important record
+savepara_file = 'model.ckpt' #NameError: name 'FLAGS' is not defined
+savepara_dir = './save_parameter/'    
+saver.save(sess,savepara_dir+savepara_file)
+then I will get 4 files
+
+checkpoint
+savepara_file.data-00000-of-00001
+savepara_file.index
+savepara_file.meta
+
+suffix  .data-00000-of-00001  .index  .meta
+
+
+saver.restore(sess, savepara_dir + savepara_file)
+print("W,b value after restore: ", sess.run([W, b]))
+
+get the right thing !!
+
+then will the global step
+
+
+if I add the global step saver.save(sess, savepara_dir + savepara_file, global_step=i)
+the savepara_file get a suffix of -i
+
+
+1. save and check the input file, don't download every time
+
+    reference: train.py download from url
